e3a6844abc851a086d076a43b0a1d551768ec1cf
shankar5885 <34203987+shankar5885@users.noreply.github.com>
2022-11-14 00:11:44 -0600

    Update Input_from_inside.ps1

--- Input_from_inside.ps1
+++ Input_from_inside.ps1
@@ -1,3 +1,4 @@
+###################################################
 #input from inside Runbook
 $servicePrincipalConnection = Get-AutomationConnection -Name "AzureRunAsConnection"       
 Connect-AzAccount -ServicePrincipal -TenantId $servicePrincipalConnection.TenantId -ApplicationId $servicePrincipalConnection.ApplicationId -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
@@ -89,7 +90,7 @@ New-AzVM -ResourceGroupName $rgname -Location $location -VM $vmconfig
 }
 
 
-
+#############################################################
 #input from outside Runbook schedule/manual trigger
 Param
 (
@@ -209,7 +210,7 @@ New-AzVM -ResourceGroupName $rgname -Location $location -VM $vmconfig
 }
 
 
-
+############################################################################
 #Input from WebHook
 
 Param
@@ -312,3 +313,169 @@ New-AzVM -ResourceGroupName $rgname -Location $location -VM $vmconfig
 else {
 	Write-Output "This runbook has not triggered by webhook"
 }
+
+
+################################################################
+#Hybrid worker in Automation account to run your jubs in VM
+# Create log analytics workspace
+#run below command
+Set-AzOperationalInsightsIntelligencePack -ResourceGroupName autoaccrg01 -WorkspaceName law001 -IntelligencePackName "AzureAutomation" -Enabled $true
+
+#take workspaceID and WorkspaceKey from log anaytics workspace
+$publicsettings = @{"WorkspaceID" = "2a059b15-d184-4c8b-821a-xxxxxxx"}
+$protectedsettings = @{"WorkspaceKey" = "ZJZ9WAxxxxxxxxxxxxxxxxXox2au/0ay635OLx28MgogXgClU6bWhxxxxxxxxxxxxxWZAuQ=="}
+#create VM
+#run below command to install agent in newly created VM
+Set-AzVMExtension -ExtensionName "MicrosoftMonitoringAgent" -ResourceGroupName newrg01 -VMName VM01 -Publisher "Microsoft.EnterpriseCloud.Monitoring" -ExtensionType "MicrosoftMonitoringAgent" -TypeHandlerVersion 1.0 -Settings $publicsettings -ProtectedSettings $protectedsettings -Location "west europe"
+
+
+#RDP to VM
+#Take Powershell
+    cd "C:\Program Files\Microsoft Monitoring Agent\Agent\AzureAutomation\7.3.1209.0\HybridRegistration"
+    Import-Module .\HybridRegistration.psd1
+    Add-HybridRunbookWorker -url "" -key "" (Url and key is from automation account, under keys)
+#hybrid worker is available in Automation account
+
+######################################################
+#VM auto start stop based on Tags
+
+param (
+
+    [Parameter(Mandatory=$true)]  
+    [String] $Action,
+
+    [Parameter(Mandatory=$false)]  
+    [String] $TagName,
+
+    [Parameter(Mandatory=$false)]
+    [String] $TagValue
+) 
+
+Connect-AzAccount -Identity
+## End of authentication
+
+## Getting all virtual machines
+
+Write-Output "---------------------------- Status ----------------------------"
+Write-Output "Getting all virtual machines from all resource groups ..."
+
+try
+{
+    if ($TagName)
+    {                    
+        $instances = Get-AzResource -TagName $TagName -TagValue $TagValue -ResourceType "Microsoft.Compute/virtualMachines"
+        
+        if ($instances)
+        {
+            $resourceGroupsContent = @()
+                                      
+            foreach ($instance in $instances)
+            {
+                $instancePowerState = (((Get-AzVM -ResourceGroupName $($instance.ResourceGroupName) -Name $($instance.Name) -Status).Statuses.Code[1]) -replace "PowerState/", "")
+
+                $resourceGroupContent = New-Object -Type PSObject -Property @{
+                    "Resource group name" = $($instance.ResourceGroupName)
+                    "Instance name" = $($instance.Name)
+                    "Instance type" = (($instance.ResourceType -split "/")[0].Substring(10))
+                    "Instance state" = ([System.Threading.Thread]::CurrentThread.CurrentCulture.TextInfo.ToTitleCase($instancePowerState))
+                    $TagName = $TagValue
+                }
+
+                $resourceGroupsContent += $resourceGroupContent
+            }
+        }
+        else
+        {
+            #Do nothing
+        }
+    }       
+
+    $resourceGroupsContent | Format-Table -AutoSize
+}
+catch
+{
+    Write-Error -Message $_.Exception
+    throw $_.Exception    
+}
+## End of getting all virtual machines
+
+$runningInstances = ($resourceGroupsContent | Where-Object {$_.("Instance state") -eq "Running" -or $_.("Instance state") -eq "Starting"})
+$deallocatedInstances = ($resourceGroupsContent | Where-Object {$_.("Instance state") -eq "Deallocated" -or $_.("Instance state") -eq "Deallocating"})
+
+## Updating virtual machines power state
+if (($runningInstances) -and ($Action -eq "Stop"))
+{
+    Write-Output "--------------------------- Updating ---------------------------"
+    Write-Output "Trying to stop virtual machines ..."
+
+    try
+    {
+        $updateStatuses = @()
+         
+        foreach ($runningInstance in $runningInstances)
+        {
+            Write-Output "$($runningInstance.("Instance name")) is shutting down ..."
+        
+            $startTime = Get-Date -Format G
+
+            $null = Stop-AzVM -ResourceGroupName $($runningInstance.("Resource group name")) -Name $($runningInstance.("Instance name")) -Force
+            
+            $endTime = Get-Date -Format G
+
+            $updateStatus = New-Object -Type PSObject -Property @{
+                "Resource group name" = $($runningInstance.("Resource group name"))
+                "Instance name" = $($runningInstance.("Instance name"))
+                "Start time" = $startTime
+                "End time" = $endTime
+            }
+            
+            $updateStatuses += $updateStatus       
+        }
+
+        $updateStatuses | Format-Table -AutoSize
+    }
+    catch
+    {
+        Write-Error -Message $_.Exception
+        throw $_.Exception    
+    }
+}
+elseif (($deallocatedInstances) -and ($Action -eq "Start"))
+{
+    Write-Output "--------------------------- Updating ---------------------------"
+    Write-Output "Trying to start virtual machines ..."
+
+    try
+    {
+        $updateStatuses = @()
+
+        foreach ($deallocatedInstance in $deallocatedInstances)
+        {                                    
+            Write-Output "$($deallocatedInstance.("Instance name")) is starting ..."
+
+            $startTime = Get-Date -Format G
+
+            $null = Start-AzVM -ResourceGroupName $($deallocatedInstance.("Resource group name")) -Name $($deallocatedInstance.("Instance name"))
+
+            $endTime = Get-Date -Format G
+
+            $updateStatus = New-Object -Type PSObject -Property @{
+                "Resource group name" = $($deallocatedInstance.("Resource group name"))
+                "Instance name" = $($deallocatedInstance.("Instance name"))
+                "Start time" = $startTime
+                "End time" = $endTime
+            }
+
+            $updateStatuses += $updateStatus
+        }
+
+        $updateStatuses | Format-Table -AutoSize
+    }
+    catch
+    {
+        Write-Error -Message $_.Exception
+        throw $_.Exception    
+    }
+}
+#### End of updating virtual machines power state
+
